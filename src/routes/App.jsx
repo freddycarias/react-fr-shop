77bc605c0e9d597689c1ce61a89f3b21beb6dfa7
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-11-29 12:53:57 -0600

    Solución al reto: componentes y rutas de React Shop

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,25 +1,39 @@
 import React from'react';
 import {BrowserRouter, Routes, Route} from'react-router-dom';
 import  Home  from'../pages/Home';
-import  Layout  from'../containers/layout';
-import  Login  from'../containers/login';
-import  RecoveryPassword  from'../containers/RecoveryPassword';
+import  Layout  from'../containers/Layout';
+import  Login  from'../pages/Login';
+import  PasswordRecovery  from'../pages/PasswordRecovery';
 import  NotFound  from'../pages/NotFound';
 import'../styles/golbal.css';
-import Header from "../components/Header"
+import SendEmail from '../pages/SendEmail';
+import NewPassword from '../pages/NewPassword';
+import MyAccount from '../pages/MyAccount';
+import CreateAccount from '../pages/CreateAccount';
+import Checkout from '../pages/Checkout';
+import Orders from '../pages/Orders';
+
+
+
 
 const App = () => {
     return (
-        <BrowserRouter>      
-            <Layout>
-                <Routes>
-                    <Route exact path='/'element={<Home/>} />
-                    <Route exact path='/login'element={<Login/>} />
-                    <Route exact path='/recovery-password'element={<RecoveryPassword/>} />
-                    <Route path='*'element={<NotFound />} />
-                </Routes>
-            </Layout>
-        </BrowserRouter>
+        <BrowserRouter>
+			<Layout>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/password-recovery" component={PasswordRecovery} />
+					<Route exact path="/send-email" component={SendEmail} />
+					<Route exact path="/new-password" component={NewPassword} />
+					<Route exact path="/account" component={MyAccount} />
+					<Route exact path="/signup" component={CreateAccount} />
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/orders" component={Orders} />
+					<Route path="*" component={NotFound} />
+				</Switch>
+			</Layout>
+		</BrowserRouter>
     )
 }
 export default App;
