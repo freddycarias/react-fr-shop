1d1c5f2a5e408f0c8a110321dee9e34f0f5c4832
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-11-29 15:00:27 -0600

    fixing the app file with the new implements of react

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -20,18 +20,18 @@ const App = () => {
     return (
         <BrowserRouter>
 			<Layout>
-				<Switch>
-					<Route exact path="/" component={Home} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/password-recovery" component={PasswordRecovery} />
-					<Route exact path="/send-email" component={SendEmail} />
-					<Route exact path="/new-password" component={NewPassword} />
-					<Route exact path="/account" component={MyAccount} />
-					<Route exact path="/signup" component={CreateAccount} />
-					<Route exact path="/checkout" component={Checkout} />
-					<Route exact path="/orders" component={Orders} />
-					<Route path="*" component={NotFound} />
-				</Switch>
+				<Routes>
+					<Route exact path="/" element={<Home/>} />
+					<Route exact path="/login" element={<Login/>} />
+					<Route exact path="/password-recovery" element={<PasswordRecovery/>} />
+					<Route exact path="/send-email" element={<SendEmail/>} />
+					<Route exact path="/new-password" element={<NewPassword/>} />
+					<Route exact path="/account" element={<MyAccount/>} />
+					<Route exact path="/signup" element={<CreateAccount/>} />
+					<Route exact path="/checkout" element={<Checkout/>} />
+					<Route exact path="/orders" element={<Orders/>} />
+					<Route path="*" element={<NotFound/>} />
+				</Routes>
 			</Layout>
 		</BrowserRouter>
     )
