a6571bc2c880461ac4e2667669fe85d766a88903
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-11-30 16:10:30 -0600

    React.useState, y estilos a mi gusto , mas el cambio a figure por un button

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import '../styles/ProductItem.scss';
 
 import iconAddCart from "@icons/bt_add_to_cart.svg";
 
+
 const ProductItem = () => {
+	const [cart, setCart] = React.useState(null);
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -12,9 +14,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<button onClick={ () => setCart(cart + 1) }>
 					<img src= {iconAddCart} alt="" />
-				</figure>
+				</button>
+				{cart}
 			</div>
 		</div>
 	);
