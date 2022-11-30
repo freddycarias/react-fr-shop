deb7c54f585fde27845db1d12ffa9ac32302a63c
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-11-30 11:09:19 -0600

    Imágenes y alias en Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { dirname } = require('path');
 
 module.exports = {
     entry: './src/index.js',
@@ -11,7 +12,18 @@ module.exports = {
     },
     mode: 'development',
     resolve:{
-        extensions: ['.js','.jsx']
+        extensions: ['.js','.jsx'],
+        alias: {
+            "@components": path.resolve(__dirname,"./src/components/"),
+            "@containers": path.resolve(__dirname,"./src/containers/"),
+            "@pages": path.resolve(__dirname,"./src/pages/"),
+            "@routes": path.resolve(__dirname,"./src/routes/"),
+            "@styles": path.resolve(__dirname,"./src/styles/"),
+            "@icons": path.resolve(__dirname,"/src/assets/icons/"),
+            "@logos": path.resolve(__dirname,"/src/assets/logos/")
+
+
+        }
     },
     module:{
         rules:[
@@ -31,6 +43,10 @@ module.exports = {
                     "css-loader",
                     "sass-loader"
                 ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                type: "asset"
             }
         ]
     },
