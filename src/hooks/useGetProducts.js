0ef42b8d197e55727a3a2d352b37033723a1a25c
Rene Carias Vasquez <freddyrenecariasvasquez@gmail.com>
2022-12-01 15:18:48 -0600

    forma correcta de crear el el useEffect

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+
 const useGetProducts = (API) => {
-    const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
+	useEffect(() => {
+		async function fetchData() {
+			const response = await axios(API);
+			setProducts(response.data);  
+		}
+		fetchData();
+		}, []);
 
-	useEffect(async () => {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])
-
-    return products;
-}
+	return products;
+};
 
 export default useGetProducts;
